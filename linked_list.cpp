f3a58ac9027a0f21cfa5e3af5849aa3f52640d65
Jo3yVX <anhquyethgvx@gmail.com>
2020-11-11 00:43:25 +0700

    Update progress

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,30 +1,64 @@
 #include <iostream>
 
 template <class T>
-class Node{
-public:
-    T data;
-    T* nextNode;
+class Node {
+	T* nextNode = nullptr;
+	T key;
 };
 
 template <class T>
-class linkedList(){
-public:
-    int totalNodes;
-    T* head = nullptr;
-    T* tail = nullptr;
-    linkedList();
-    ~linkedList();
-    void pushBack(Node n);
-    void pushFront(Node n);
-    void addAfter(Node origin, Node n);
-    void popBack();
-    void popFront();
-    bool isEmpty();
-    int Size();
-};
-int main()
-{
+class LinkedList {
+	Node<T>* head;
+	Node<T>* tail;
 
-    return 0;
+public:
+	LinkedList();
+	~LinkedList();
+	int length;
+	bool isEmpty();
+	
+	void pushFront(T value);
+	T topFront();
+	void popFront();
+	void pushBack(T value);
+	T topBack();
+	void popBack();
+	bool find(T value);
+	void erase(T value);
+	void addBefore(Node node, T value);
+	void addAfter(Node node, T value);
+	
+};
+
+template <class T>
+LinkedList<T>::LinkedList() {
+	length = 0;
+	head = nullptr;
+	tail = nullptr;
+}
+
+template <class T>
+LinkedList<T>::~LinkedList() {
+
+}
+
+template <class T>
+bool LinkedList<T>::isEmpty() {
+	if (head == nullptr) return true;
+	return false;
+}
+
+template <class T>
+void LinkedList<T>::pushFront(T value) {
+	Node* newNode = new Node;
+	newNode->key = value;
+	if (this->isEmpty()) {
+		
+	}
+}
+
+int main() {
+	LinkedList<int> ll;
+	std::cout << std::boolalpha << ll.isEmpty() << std::endl;
+	return 0;
 }
